eda2ccb4542bc542f489524b6aa093925a15b03b
Noel Madayag Jr <nmadayag@bu.edu>
2024-04-29 03:32:05 -0400

    initialized the alu

---
+++ RISC-V CPU/RISC-V CPU.srcs/sources_1/new/ALU.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/29/2024 03:29:22 AM
+// Design Name: 
+// Module Name: ALU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU(op, a, b, out);
+
+input   [3:0]   op;
+input   [31:0]  a;
+input   [31:0]  b;
+
+output  [31:0]  out;
+
+endmodule
